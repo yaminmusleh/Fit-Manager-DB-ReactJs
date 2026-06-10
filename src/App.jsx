c12daab1a80d849ef3db120224f1daad390a1ffc
yaminmusleh <yaminaktham3@gmail.com>
2026-06-10 22:45:39 +0300

    some fixing for some issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import { RouterProvider } from 'react-router-dom'
-import router from './router'
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import router from "./router";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export default function App() {
   return (
-    <RouterProvider router = {router}/>
-  )
+    <>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </>
+  );
 }
